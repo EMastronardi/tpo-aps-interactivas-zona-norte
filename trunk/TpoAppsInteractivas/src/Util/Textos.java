a4130f71b2b524055404a723d30040d8eaab4f3d
d.a.zanotti <d.a.zanotti@26e0bbe2-a1fa-7ea8-7f35-f9f341cf34c7>
2012-06-03 23:37:22 +0000

    cambios en la vista

---
+++ trunk/TpoAppsInteractivas/src/Util/Textos.java
@@ -0,0 +1,53 @@
+package Util;
+
+public class Textos {
+
+	public final static String ERROR = "Error";
+	public final static String AVISO = "Aviso";
+	public final static String ALTA = "Alta";
+	public final static String BAJA = "Eliminar";
+	public final static String MODIFICAR = "Modificar";
+	public static final String LISTADO = "Listado";
+	public final static String CONFIRMAR = "Confirmar";
+	public final static String PREGUNTA_BAJA = "�Esta seguro que desea eliminar el resgistro?";
+	public final static String PREGUNTA_MODIFICAR = "�Esta seguro que desea modificar el resgistro?";
+	
+	public final static String FALTANTES = "Faltan completar campos";
+	
+	public final static String ALTA_OK = "El alta se realiz� correctamente";
+	public final static String ALTA_ERROR = "El alta no pudo llevarse a cabo";
+	public final static String BAJA_OK = "La baja se realiz� correctamente";
+	public final static String BAJA_ERROR = "La baja no pudo llevarse a cabo";
+	public final static String MODIFICACION_OK = "La modificaci�n se realiz� correctamente";
+	public final static String MODIFICACION_ERROR = "La modificaci�n no pudo llevarse a cabo";
+	
+	/*
+	public final static String USUARIO_VACIO = "El campo Usuario no puede estar vacio.";
+	public final static String PASSWORD_VACIO = "El campo Passowrd no puede estar vacio.";
+	public final static String DESCRIPCION_VACIO = "El campo Descripci�n no puede estar vacio.";
+	public final static String ESTADO_VACIO = "El campo Estado no puede estar vacio.";
+	public final static String PROYECTO_VACIO = "El campo Proyecto no puede estar vacio.";
+	public final static String REQUERIMIENTO_VACIO = "El campo Requerimiento no puede estar vacio.";
+	public final static String SALIR_SISTEMA = "Confirmar Salida";
+	public final static String SALIDA_SISTEMA = "�Realmente desea salir del Sistema?";
+	
+	public final static String HORA_ESTIMADA_SOLO_NUMERO = "El campo Hora estimada solo acepta n�meros";
+	
+	public final static String NO_REQUERIMIENTOS = "No se encontraron Requerimientos con los parametros indicados.";
+	
+	public final static String REPORTE_TIPO = "No se selecciono tipo de reporte";
+	public final static String REPORTE_FECHA = "No se selecciono fechas";
+	
+	public final static String CANCELAR_CARGA_HORAS = "Cancelar carga de horas";
+	public final static String SALIDA_CARGA_HORAS = "�Realmente desea cancelar la carga de horas?";
+	public final static String HORAS_DUPLICADAS = "Existen registros de horas duplicados. Por favor corrija los datos.";
+	public final static String NUEVO_REGISTRO = "Imposible insertar un nuevo registro. Realizar la carga de a uno a la vez.";
+	public final static String SELECCION_SEMANA = "Por favor seleccione una semana antes de continuar";
+	public final static String GUARDAR_HORAS_ERROR = "El registro de horas no pudo llevarse a cabo.";
+	public final static String GUARDAR_HORAS_OK = "El registro de horas se realiz� correctamente.";
+	public final static String HORAS_EXCEDIDAS = "Se excedieron las 40 horas semanales.";
+	
+	public final static String CONEXION_JDBC_ERROR = "Se ha producido un error al querer conectarse a la base de datos.";
+	public final static String GENERAR_REPORTE_ERROR = "Se produjo un error al generar el reporte solicitado.";*/
+}
+
---
+++ trunk/TpoAppsInteractivas/src/Vista/PopUp_vw.java
@@ -0,0 +1,56 @@
+package Vista;
+
+import javax.swing.GroupLayout;
+import javax.swing.JComponent;
+import javax.swing.WindowConstants;
+
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class PopUp_vw extends javax.swing.JFrame {
+
+	{
+		//Set Look & Feel
+		try {
+			javax.swing.UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	/**
+	* Auto-generated main method to display this JFrame
+	*/
+		
+	public PopUp_vw() {
+		super();
+		initGUI();
+	}
+	
+	private void initGUI() {
+		try {
+			GroupLayout thisLayout = new GroupLayout((JComponent)getContentPane());
+			getContentPane().setLayout(thisLayout);
+			thisLayout.setHorizontalGroup(thisLayout.createParallelGroup());
+			thisLayout.setVerticalGroup(thisLayout.createParallelGroup());
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			pack();
+			setSize(400, 300);
+		} catch (Exception e) {
+		    //add your error handling code here
+			e.printStackTrace();
+		}
+	}
+
+}
