65acfca6adbfe95bee893a89d459c3b8d64c9625
d.a.zanotti <d.a.zanotti@26e0bbe2-a1fa-7ea8-7f35-f9f341cf34c7>
2012-06-05 01:14:10 +0000

    termine con la ABM de cliente Industrial

--- trunk/TpoAppsInteractivas/src/Vista/ClienteIndustrial_vw.java
+++ trunk/TpoAppsInteractivas/src/Vista/ClienteIndustrial_vw.java
@@ -1,10 +1,16 @@
 package Vista;
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle;
 import javax.swing.SwingConstants;
@@ -12,7 +18,9 @@ import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 import javax.swing.SwingUtilities;
 
-
+import Controlador.Sistema;
+import Modelo.IndustrialView;
+import Util.Textos;
 /**
 * This code was edited or generated using CloudGarden's Jigloo
 * SWT/Swing GUI Builder, which is free for non-commercial
@@ -26,12 +34,16 @@ import javax.swing.SwingUtilities;
 * LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
 */
 public class ClienteIndustrial_vw extends javax.swing.JFrame {
-	private JLabel jlNombre;
+	
+	private Sistema sistema;
+	private int operacion;
+	
+	private JLabel jlRA;
 	private JLabel jlDepto;
 	private JTextField jtIIBB;
-	private JLabel jlCO;
+	private JLabel jlCF;
 	private JLabel jlIIBB;
-	private JTextField jtCO;
+	private JTextField jtCF;
 	private JButton jbCancelar;
 	private JButton jbAceptar;
 	private JTextField jtProvincia;
@@ -41,8 +53,8 @@ public class ClienteIndustrial_vw extends javax.swing.JFrame {
 	private JTextField jtPiso;
 	private JTextField jtAltura;
 	private JTextField jtCalle;
-	private JTextField jtApellido;
-	private JTextField jtNombre;
+	private JTextField jtCUIT;
+	private JTextField jtRA;
 	private JTextField jtNroCliente;
 	private JLabel jlProvincia;
 	private JLabel jlLocalidad;
@@ -50,25 +62,21 @@ public class ClienteIndustrial_vw extends javax.swing.JFrame {
 	private JLabel jlPiso;
 	private JLabel jlAltura;
 	private JLabel jlCalle;
-	private JLabel jlApellido;
+	private JLabel jlCUIT;
 	private JLabel jlNroCliente;
 
 	/**
 	* Auto-generated main method to display this JFrame
 	*/
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				ClienteIndustrial_vw inst = new ClienteIndustrial_vw();
-				inst.setLocationRelativeTo(null);
-				inst.setVisible(true);
-			}
-		});
-	}
 	
-	public ClienteIndustrial_vw() {
+	public ClienteIndustrial_vw(Sistema sistema, int op) {
 		super();
+		this.sistema = sistema;
+		this.operacion = op;
 		initGUI();
+		addAcciones();
+		setLocationRelativeTo(null);
+		setVisible(true);
 	}
 	
 	private void initGUI() {
@@ -76,18 +84,27 @@ public class ClienteIndustrial_vw extends javax.swing.JFrame {
 			GroupLayout thisLayout = new GroupLayout((JComponent)getContentPane());
 			getContentPane().setLayout(thisLayout);
 			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			
+			//Metodo para capturar el evento de salida y volver a instanciar el sistema
+			addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent e) {
+					dispose();
+					new MenuPrincipal_vw(sistema);
+				}
+			});
+			
 			this.setTitle("Cliente Industrial");
 			{
-				jlNombre = new JLabel();
-				jlNombre.setText("Razon Social:");
+				jlRA = new JLabel();
+				jlRA.setText("Razon Social:");
 			}
 			{
 				jlNroCliente = new JLabel();
 				jlNroCliente.setText("Nro Cliente:");
 			}
 			{
-				jlApellido = new JLabel();
-				jlApellido.setText("CUIT:");
+				jlCUIT = new JLabel();
+				jlCUIT.setText("CUIT:");
 			}
 			{
 				jlCalle = new JLabel();
@@ -119,20 +136,21 @@ public class ClienteIndustrial_vw extends javax.swing.JFrame {
 			}
 			{
 				jtNroCliente = new JTextField();
+				jtNroCliente.setEnabled(false);
 			}
 			{
 				jlIIBB = new JLabel();
 				jlIIBB.setText("Ingresos Brutos:");
 			}
 			{
-				jlCO = new JLabel();
-				jlCO.setText("Condicion Fiscal:");
+				jlCF = new JLabel();
+				jlCF.setText("Condicion Fiscal:");
 			}
 			{
 				jtIIBB = new JTextField();
 			}
 			{
-				jtCO = new JTextField();
+				jtCF = new JTextField();
 			}
 			{
 				jbAceptar = new JButton();
@@ -143,10 +161,10 @@ public class ClienteIndustrial_vw extends javax.swing.JFrame {
 				jbCancelar.setText("Cancelar");
 			}
 			{
-				jtNombre = new JTextField();
+				jtRA = new JTextField();
 			}
 			{
-				jtApellido = new JTextField();
+				jtCUIT = new JTextField();
 			}
 			{
 				jtCalle = new JTextField();
@@ -176,12 +194,12 @@ public class ClienteIndustrial_vw extends javax.swing.JFrame {
 				    .addComponent(jtNroCliente, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE))
 				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
 				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-				    .addComponent(jlNombre, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
-				    .addComponent(jtNombre, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE))
+				    .addComponent(jlRA, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(jtRA, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE))
 				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
 				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-				    .addComponent(jtApellido, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
-				    .addComponent(jlApellido, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
+				    .addComponent(jtCUIT, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(jlCUIT, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
 				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
 				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
 				    .addComponent(jtCalle, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
@@ -216,8 +234,8 @@ public class ClienteIndustrial_vw extends javax.swing.JFrame {
 				    .addComponent(jlIIBB, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
 				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
 				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-				    .addComponent(jtCO, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
-				    .addComponent(jlCO, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
+				    .addComponent(jtCF, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(jlCF, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
 				.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED, 1, Short.MAX_VALUE)
 				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
 				    .addComponent(jbAceptar, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
@@ -226,7 +244,7 @@ public class ClienteIndustrial_vw extends javax.swing.JFrame {
 			thisLayout.setHorizontalGroup(thisLayout.createSequentialGroup()
 				.addContainerGap()
 				.addGroup(thisLayout.createParallelGroup()
-				    .addComponent(jlCO, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(jlCF, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
 				    .addComponent(jlIIBB, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
 				    .addComponent(jlProvincia, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
 				    .addComponent(jlLocalidad, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
@@ -235,8 +253,8 @@ public class ClienteIndustrial_vw extends javax.swing.JFrame {
 				    .addComponent(jlPiso, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
 				    .addComponent(jlAltura, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
 				    .addComponent(jlCalle, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
-				    .addComponent(jlApellido, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
-				    .addComponent(jlNombre, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(jlCUIT, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(jlRA, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
 				    .addComponent(jlNroCliente, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE))
 				.addGap(61)
 				.addGroup(thisLayout.createParallelGroup()
@@ -251,7 +269,7 @@ public class ClienteIndustrial_vw extends javax.swing.JFrame {
 				        .addGap(25)
 				        .addComponent(jbCancelar, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE))
 				    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
-				        .addComponent(jtCO, GroupLayout.PREFERRED_SIZE, 232, GroupLayout.PREFERRED_SIZE)
+				        .addComponent(jtCF, GroupLayout.PREFERRED_SIZE, 232, GroupLayout.PREFERRED_SIZE)
 				        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 0, Short.MAX_VALUE))
 				    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
 				        .addComponent(jtIIBB, GroupLayout.PREFERRED_SIZE, 232, GroupLayout.PREFERRED_SIZE)
@@ -278,19 +296,19 @@ public class ClienteIndustrial_vw extends javax.swing.JFrame {
 				        .addComponent(jtCalle, GroupLayout.PREFERRED_SIZE, 232, GroupLayout.PREFERRED_SIZE)
 				        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 0, Short.MAX_VALUE))
 				    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
-				        .addComponent(jtApellido, GroupLayout.PREFERRED_SIZE, 232, GroupLayout.PREFERRED_SIZE)
+				        .addComponent(jtCUIT, GroupLayout.PREFERRED_SIZE, 232, GroupLayout.PREFERRED_SIZE)
 				        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 0, Short.MAX_VALUE))
 				    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
-				        .addComponent(jtNombre, GroupLayout.PREFERRED_SIZE, 232, GroupLayout.PREFERRED_SIZE)
+				        .addComponent(jtRA, GroupLayout.PREFERRED_SIZE, 232, GroupLayout.PREFERRED_SIZE)
 				        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 0, Short.MAX_VALUE)))
 				.addContainerGap(42, 42));
-			thisLayout.linkSize(SwingConstants.VERTICAL, new Component[] {jtCP, jtDepto, jtPiso, jtAltura, jtCalle, jtApellido, jtNombre, jtNroCliente});
-			thisLayout.linkSize(SwingConstants.VERTICAL, new Component[] {jtCO, jtIIBB, jtProvincia, jtLocalidad});
+			thisLayout.linkSize(SwingConstants.VERTICAL, new Component[] {jtCP, jtDepto, jtPiso, jtAltura, jtCalle, jtCUIT, jtRA, jtNroCliente});
+			thisLayout.linkSize(SwingConstants.VERTICAL, new Component[] {jtCF, jtIIBB, jtProvincia, jtLocalidad});
 			thisLayout.linkSize(SwingConstants.VERTICAL, new Component[] {jbAceptar, jbCancelar});
-			thisLayout.linkSize(SwingConstants.HORIZONTAL, new Component[] {jtCO, jtIIBB, jtProvincia});
-			thisLayout.linkSize(SwingConstants.HORIZONTAL, new Component[] {jlProvincia, jlIIBB, jlCO});
-			thisLayout.linkSize(SwingConstants.HORIZONTAL, new Component[] {jlApellido, jlNombre, jlNroCliente, jlCalle, jlAltura, jlPiso, jlDepto, jlCP, jlLocalidad});
-			thisLayout.linkSize(SwingConstants.HORIZONTAL, new Component[] {jtNombre, jtApellido, jtCalle, jtAltura, jtPiso, jtDepto, jtCP, jtLocalidad});
+			thisLayout.linkSize(SwingConstants.HORIZONTAL, new Component[] {jtCF, jtIIBB, jtProvincia});
+			thisLayout.linkSize(SwingConstants.HORIZONTAL, new Component[] {jlProvincia, jlIIBB, jlCF});
+			thisLayout.linkSize(SwingConstants.HORIZONTAL, new Component[] {jlCUIT, jlRA, jlNroCliente, jlCalle, jlAltura, jlPiso, jlDepto, jlCP, jlLocalidad});
+			thisLayout.linkSize(SwingConstants.HORIZONTAL, new Component[] {jtRA, jtCUIT, jtCalle, jtAltura, jtPiso, jtDepto, jtCP, jtLocalidad});
 			thisLayout.linkSize(SwingConstants.HORIZONTAL, new Component[] {jbAceptar, jbCancelar});
 			pack();
 			this.setSize(492, 394);
@@ -299,5 +317,117 @@ public class ClienteIndustrial_vw extends javax.swing.JFrame {
 			e.printStackTrace();
 		}
 	}
-
+	
+	private void addAcciones(){
+		
+		jbAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(operacion==0){//Cliente nuevo
+					if(!jtRA.getText().isEmpty()  && !jtCUIT.getText().isEmpty() && !jtCalle.getText().isEmpty() &&
+							!jtAltura.getText().isEmpty() && !jtPiso.getText().isEmpty() && !jtDepto.getText().isEmpty() &&
+							!jtCP.getText().isEmpty() && !jtProvincia.getText().isEmpty() && !jtLocalidad.getText().isEmpty() && !jtIIBB.getText().isEmpty() && !jtCF.getText().isEmpty()){
+						
+							if(sistema.altaClienteIndustrial(jtCalle.getText(), Integer.parseInt(jtAltura.getText()),  Integer.parseInt(jtPiso.getText()), jtDepto.getText(), jtCP.getText(), jtLocalidad.getText(), jtProvincia.getText(), jtRA.getText(), jtCUIT.getText(), jtIIBB.getText(), jtCF.getText())){
+							JOptionPane.showMessageDialog(new PopUp_vw(), 
+									Textos.ALTA_OK, Textos.AVISO, JOptionPane.INFORMATION_MESSAGE);
+							//Vuelvo al menu principal
+							dispose();
+							new MenuPrincipal_vw(sistema);
+							}
+							else{
+								JOptionPane.showMessageDialog(new PopUp_vw(), 
+										Textos.ALTA_ERROR, Textos.ERROR, JOptionPane.ERROR_MESSAGE);
+							}
+					}
+					else{
+						JOptionPane.showMessageDialog(new PopUp_vw(), 
+								Textos.FALTANTES, Textos.ERROR, JOptionPane.ERROR_MESSAGE);
+					}
+				
+				}
+				else{
+					if(operacion == 1){//Modificacion
+						if(!jtRA.getText().isEmpty()  && !jtCUIT.getText().isEmpty() && !jtCalle.getText().isEmpty() &&
+								!jtAltura.getText().isEmpty() && !jtPiso.getText().isEmpty() && !jtDepto.getText().isEmpty() &&
+								!jtCP.getText().isEmpty() && !jtProvincia.getText().isEmpty() && !jtLocalidad.getText().isEmpty() && !jtIIBB.getText().isEmpty() && !jtCF.getText().isEmpty()){
+							
+							if(sistema.modificarClienteIndustrial(Integer.parseInt(jtNroCliente.getText()),jtCalle.getText(),  Integer.parseInt(jtAltura.getText()),  Integer.parseInt(jtPiso.getText()), jtDepto.getText(), jtCP.getText(), jtLocalidad.getText(), jtProvincia.getText(), jtRA.getText(), jtCUIT.getText(), jtIIBB.getText(), jtCF.getText())){
+								JOptionPane.showMessageDialog(new PopUp_vw(), 
+										Textos.MODIFICACION_OK, Textos.AVISO, JOptionPane.INFORMATION_MESSAGE);
+								//Vuelvo al menu principal
+								dispose();
+								new MenuPrincipal_vw(sistema);
+							}
+							else{
+								JOptionPane.showMessageDialog(new PopUp_vw(), 
+										Textos.MODIFICACION_ERROR, Textos.ERROR, JOptionPane.ERROR_MESSAGE);
+							}
+						}
+						else{
+							JOptionPane.showMessageDialog(new PopUp_vw(), 
+									Textos.FALTANTES, Textos.ERROR, JOptionPane.ERROR_MESSAGE);
+						}
+					}//Baja
+					else{
+						if(sistema.bajaClienteIndustrial(Integer.parseInt(jtNroCliente.getText()), null, null, null, null, null, null, null, null, null, null, null)){
+							JOptionPane.showMessageDialog(new PopUp_vw(), 
+									Textos.BAJA_OK, Textos.AVISO, JOptionPane.INFORMATION_MESSAGE);
+							//Vuelvo al menu principal
+							dispose();
+							new MenuPrincipal_vw(sistema);
+						}
+						else{
+							JOptionPane.showMessageDialog(new PopUp_vw(), 
+									Textos.BAJA_ERROR, Textos.ERROR, JOptionPane.ERROR_MESSAGE);
+						}
+					}	
+				}
+			}	
+		});
+		
+		jbCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				new MenuPrincipal_vw(sistema);
+			}
+		});
+	}
+	
+	public void cargarIndustrial(IndustrialView i){
+		
+		jtNroCliente.setText(String.valueOf(i.getNroCliente()));
+		jtRA.setText(i.getRazonSocial());
+		jtCUIT.setText(i.getCuit());
+		jtCalle.setText(i.getCalle());
+		jtAltura.setText(String.valueOf(i.getAltura()));
+		jtPiso.setText(String.valueOf(i.getPiso()));
+		jtDepto.setText(i.getDepartamento());
+		jtCP.setText(i.getCodigoPostal());
+		jtLocalidad.setText(i.getLocalidad());
+		jtProvincia.setText(i.getProvincia());
+		jtCF.setText(i.getCategoria());
+		jtIIBB.setText(i.getIngresosBrutos());
+		
+		if(operacion == 1)
+			jbAceptar.setText("Modificar");
+		else
+			jbAceptar.setText("Baja");	
+	}
+	
+	public void inhabilitarCampos(){
+		
+		jtNroCliente.setEnabled(false);
+		jtRA.setEnabled(false);
+		jtCUIT.setEnabled(false);
+		jtCalle.setEnabled(false);
+		jtAltura.setEnabled(false);
+		jtPiso.setEnabled(false);
+		jtDepto.setEnabled(false);
+		jtCP.setEnabled(false);
+		jtLocalidad.setEnabled(false);
+		jtProvincia.setEnabled(false);
+		jtCF.setEnabled(false);
+		jtIIBB.setEnabled(false);
+	}
+		
 }
