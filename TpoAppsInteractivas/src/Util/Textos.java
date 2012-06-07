bd1b4f9dc1a3a5c90db8b2e15b9de8f901076de2
d.a.zanotti <d.a.zanotti@26e0bbe2-a1fa-7ea8-7f35-f9f341cf34c7>
2012-06-07 03:50:44 +0000

    Ultimos retoques

--- TpoAppsInteractivas/src/Util/Textos.java
+++ TpoAppsInteractivas/src/Util/Textos.java
@@ -9,23 +9,23 @@ public class Textos {
 	public final static String MODIFICAR = "Modificar";
 	public static final String LISTADO = "Listado";
 	public final static String CONFIRMAR = "Confirmar";
-	public final static String PREGUNTA_BAJA = "�Esta seguro que desea eliminar el resgistro?";
-	public final static String PREGUNTA_MODIFICAR = "�Esta seguro que desea modificar el resgistro?";
+	public final static String PREGUNTA_BAJA = "¿Esta seguro que desea eliminar el resgistro?";
+	public final static String PREGUNTA_MODIFICAR = "¿Esta seguro que desea modificar el resgistro?";
 	
 	public final static String FALTANTES = "Faltan completar campos";
 	
-	public final static String ALTA_OK = "El alta se realiz� correctamente";
+	public final static String ALTA_OK = "El alta se realizo correctamente";
 	public final static String ALTA_ERROR = "El alta no pudo llevarse a cabo";
-	public final static String BAJA_OK = "La baja se realiz� correctamente";
+	public final static String BAJA_OK = "La baja se realizo correctamente";
 	public final static String BAJA_ERROR = "La baja no pudo llevarse a cabo";
-	public final static String MODIFICACION_OK = "La modificaci�n se realiz� correctamente";
-	public final static String MODIFICACION_ERROR = "La modificaci�n no pudo llevarse a cabo";
+	public final static String MODIFICACION_OK = "La modificacion se realizo correctamente";
+	public final static String MODIFICACION_ERROR = "La modificacion no pudo llevarse a cabo";
 	
 	public final static String MEDICION_OK = "La medicion se grabo correctamente";
 	public final static String MEDICION_ERROR = "La medicion no se pudo grabar correctamente";
 	
-	public final static String CONFIRMAR_FACTURACION = "Esta seguro que desea generar el proceso de facturación";
-	public final static String FACTURACION_OK = "El proceso de facturacion se pudo generar correctamente";
+	public final static String CONFIRMAR_FACTURACION = "Esta seguro que desea generar el proceso de facturacion";
+	public final static String FACTURACION_OK = "El proceso de facturacion se genero correctamente";
 	public final static String FACTURACION_ERROR = "El proceso de facturacion no se genero correctamente";
 	
 }
