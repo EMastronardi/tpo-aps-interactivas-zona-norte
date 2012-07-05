326f636a3d2993ac8a6ef5bc79aa42f8fecdef39
d.a.zanotti <d.a.zanotti@26e0bbe2-a1fa-7ea8-7f35-f9f341cf34c7>
2012-07-05 03:55:34 +0000

    persistencia facturacion

--- trunk/TpoAppsInteractivas/src/Persistencia/AdmPersistenciaFactura.java
+++ trunk/TpoAppsInteractivas/src/Persistencia/AdmPersistenciaFactura.java
@@ -2,4 +2,20 @@ package Persistencia;
 
 public class AdmPersistenciaFactura {
 
+	private static AdmPersistenciaFactura instancia;
+	
+	private AdmPersistenciaFactura()
+	{
+		
+	}
+	
+	public static AdmPersistenciaFactura getInstancia()
+	{
+		if (instancia == null)
+			instancia = new AdmPersistenciaFactura();
+		return instancia;
+	}
+	
+	
+	
 }
