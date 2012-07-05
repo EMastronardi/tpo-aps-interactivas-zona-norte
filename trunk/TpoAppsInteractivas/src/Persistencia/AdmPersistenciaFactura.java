3db5b353c694d13617e23d106c28167645eec45b
d.a.zanotti <d.a.zanotti@26e0bbe2-a1fa-7ea8-7f35-f9f341cf34c7>
2012-07-05 05:41:22 +0000

    termine la puta persistencia de facturacion

--- trunk/TpoAppsInteractivas/src/Persistencia/AdmPersistenciaFactura.java
+++ trunk/TpoAppsInteractivas/src/Persistencia/AdmPersistenciaFactura.java
@@ -1,5 +1,15 @@
 package Persistencia;
 
+import java.awt.ItemSelectable;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+
+import Modelo.Factura;
+import Modelo.ItemFactura;
+import Persistencia.AdmPersistenciaItemFactura;
+
 public class AdmPersistenciaFactura {
 
 	private static AdmPersistenciaFactura instancia;
@@ -16,6 +26,35 @@ public class AdmPersistenciaFactura {
 		return instancia;
 	}
 	
-	
+	public void insert(Object o) 
+	{
+		try
+		{
+			Factura f = (Factura)o;
+			Connection con = PoolConnection.getPoolConnection().getConnection();
+			PreparedStatement s = con.prepareStatement("insert into Facturas(nroFactura,metrosCubicosConsumidos,fecha,nroCliente) values (?,?,?,?)");
+			//agregar campos
+			s.setInt(1, f.getNroFactura());
+			s.setFloat(2, f.getMetrosCubicosConsumidos());
+			s.setDate(3, (Date) f.getFecha());
+			s.setInt(4, f.getCliente().getNroCliente());
+			
+			//Persistir items de factura
+			ArrayList<ItemFactura> aux = f.getItems();
+			Integer nroItem = 0; 
+			for(ItemFactura item : aux){
+				AdmPersistenciaItemFactura.getInstancia().insert(nroItem + 1,item, f.getNroFactura());
+			}
+			
+			s.execute();
+			PoolConnection.getPoolConnection().realeaseConnection(con);
+		}
+		catch (Exception e)
+		{
+			System.out.println();
+		}
+		
+
+	}
 	
 }
--- trunk/TpoAppsInteractivas/src/Persistencia/AdmPersistenciaItemFactura.java
+++ trunk/TpoAppsInteractivas/src/Persistencia/AdmPersistenciaItemFactura.java
@@ -1,5 +1,48 @@
 package Persistencia;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+
+import Modelo.ItemFactura;
+
 public class AdmPersistenciaItemFactura {
 
+	private static AdmPersistenciaItemFactura instancia;
+	
+	private AdmPersistenciaItemFactura()
+	{
+		
+	}
+	
+	public static AdmPersistenciaItemFactura getInstancia()
+	{
+		if (instancia == null)
+			instancia = new AdmPersistenciaItemFactura();
+		return instancia;
+	}
+	
+	public void insert(Integer nroItem, Object o, Integer nroFactura) 
+	{
+		try
+		{
+			ItemFactura items = (ItemFactura)o;
+			Connection con = PoolConnection.getPoolConnection().getConnection();
+			PreparedStatement s = con.prepareStatement("insert into ItemsFactura(nroItemFactura,nombre,valor,nroFactura) values (?,?,?,?)");
+			//agregar campos
+			s.setInt(1, nroItem);
+			s.setString(2, items.getNombre());
+			s.setFloat(3, items.getValor());
+			s.setInt(4, nroFactura);
+			s.execute();
+			PoolConnection.getPoolConnection().realeaseConnection(con);
+		}
+		catch (Exception e)
+		{
+			System.out.println();
+		}
+		
+
+	}
+	
 }
